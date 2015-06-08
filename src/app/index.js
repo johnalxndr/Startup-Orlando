4a8a1adb9fc2ddf4d006ff81d351a66d93b8863a
John Alexander <jalexander788@gmail.com>
2015-06-08 08:24:16 -0400

    adding twitter widget with styling

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,15 @@
 'use strict';
 
 angular.module('startupOrlando', ['ngAnimate', 'restangular', 'ui.router', 'ui.bootstrap'])
-  .config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      });
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'app/main/main.html',
+                controller: 'MainCtrl'
+            });
 
-    $urlRouterProvider.otherwise('/');
-  })
+        $urlRouterProvider.otherwise('/');
+    })
+
 ;
